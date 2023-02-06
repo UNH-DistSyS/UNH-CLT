f708cdd7612f1420edbeeaad986bb8261cd3745f
Marielle Webster <mlwebster2@gmail.com>
2023-02-06 12:37:16 -0500

    adding another test to complete

--- measurement/measurement_test.go
+++ measurement/measurement_test.go
@@ -7,10 +7,16 @@ import (
 	"github.com/UNH-DistSyS/UNH-CLT/ids"
 )
 
+func NewMeasurement(listSizeTestParam int) *Measurement {
+	m := CreateMeasurement(ids.GetIDFromFlag(), "test", listSizeTestParam)
+	//view stats
+	fmt.Printf("New Measurement: \n\tNodeId: %d,\n\tcsvPrefix: %s, \n\tlistSize: %d\n", m.clientId.Int(), m.prefix, len(m.data))
+	return m
+}
+
 func TestCreateMeasurement(t *testing.T) {
 	listSizeTestParam := 10
-
-	m := CreateMeasurement(ids.GetIDFromFlag(), "test", listSizeTestParam)
+	m := NewMeasurement(listSizeTestParam)
 
 	if m.fileCounter != 0 {
 		t.Fatalf("Measurement structure file counter not initialized to 0")
@@ -18,10 +24,19 @@ func TestCreateMeasurement(t *testing.T) {
 	if len(m.data) != int(listSizeTestParam) {
 		t.Fatalf("length of initialized list is: %d, exepcted: %d", len(m.data), listSizeTestParam)
 	}
-	//view stats
-	fmt.Printf("New Measurement: \n\tNodeId: %d,\n\tcsvPrefix: %s, \n\tlistSize: %d\n", m.clientId.Int(), m.prefix, len(m.data))
+}
+
+func TestEpoch(t *testing.T) {
+	//TODO: test that epoch is accurate to Jan 1 2023 0:00:00
 }
 
 func TestAddMeasurementOnce(t *testing.T) {
+	//listSizeTestParam := 10
+	//fakeNodeId := ids.GetIDFromFlag()
+	//m := NewMeasurement(listSizeTestParam)
 
+	//IN PROGRESS
+	//m.AddMeasurement(int64(100), &fakeNodeId, )
+
+	//if m.data[0]
 }
