aa08be0baec3f2d391c84ab8b0fd2f6abde6cc2c
Marielle Webster <mlwebster2@gmail.com>
2023-02-08 17:29:58 -0500

    updated tests

--- measurement/measurement.go
+++ measurement/measurement.go
@@ -4,45 +4,79 @@ package measurement
 //object to update/record data
 
 import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/UNH-DistSyS/UNH-CLT/ids"
 )
 
-var _startEpoch = (time.Date(2023, 0, 0, 0, 0, 0, 0, time.UTC)).UnixMicro()
+var _startEpoch = (time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC)).UnixMicro()
 
 type Measurement struct {
-	clientId    *ids.ID
-	prefix      string
-	data        []measurementRow
+	clientId *ids.ID
+	prefix   string
+	data     []measurementRow
+
+	//internal variables for tracking
+	listLength  int
+	rowCounter  int
 	fileCounter int
 }
 
 type measurementRow struct {
-	round  int64   //round ID
-	nodeId *ids.ID //remote node ID that is being measured
-	start  int64   //begin time, in microseconds
-	end    int64   //end time, in microseconds
+	thisNodeId   *ids.ID //id of node using Measurement
+	round        int64   //round ID
+	remoteNodeId *ids.ID //remote node ID that is being measured
+	start        int64   //begin time, in microseconds
+	end          int64   //end time, in microseconds
 }
 
 func (m *Measurement) AddMeasurement(roundNumber int64, remoteNodeID *ids.ID, startTime int64, endTime int64) {
-	row := measurementRow{
-		round:  roundNumber,
-		nodeId: remoteNodeID,
-		start:  startTime,
-		end:    endTime,
-	}
-	m.data = append(m.data, row)
+	modifiedStart := startTime - _startEpoch
+	modifiedEnd := endTime - _startEpoch
 
-	//TODO: flush logic with file counter
+	row := measurementRow{
+		thisNodeId:   m.clientId,
+		round:        roundNumber,
+		remoteNodeId: remoteNodeID,
+		start:        modifiedStart,
+		end:          modifiedEnd,
+	}
+	m.data[m.rowCounter] = row
+	m.rowCounter++
+
+	//flush list to csv if full
+	if m.rowCounter == m.listLength {
+		go flush(m.data, m.prefix, m.fileCounter)
+
+		//reset internal list and update variables
+		m.rowCounter = 0
+		m.fileCounter++
+		m.data = make([]measurementRow, m.listLength)
+	}
 }
 
 /*
 * Measurement should flush data to a file after
-a certain threshold is reached
+a certain threshold is reached.
 */
-func flush() {
+func flush(data []measurementRow, prefix string, counter int) {
+	fileName := prefix + "_" + strconv.Itoa(counter) + ".csv"
+	file, err := os.Create(fileName)
+	defer file.Close()
+	if err != nil {
+		//TODO: decide output
+		log.Fatalf("Failed to create/open file %s", fileName)
+	}
 
+	w := csv.NewWriter(file)
+	for _, item := range data {
+		w.Write([]string{item.thisNodeId.String(), strconv.FormatInt(item.round, 10), item.remoteNodeId.String(), strconv.FormatInt(item.start, 10), strconv.FormatInt(item.end, 10)})
+	}
+	w.Flush()
 }
 
 func CreateMeasurement(nodeId *ids.ID, csvPrefix string, listSize int) *Measurement {
@@ -50,6 +84,8 @@ func CreateMeasurement(nodeId *ids.ID, csvPrefix string, listSize int) *Measurem
 		clientId:    nodeId,
 		prefix:      csvPrefix,
 		data:        make([]measurementRow, listSize),
+		listLength:  listSize,
+		rowCounter:  0,
 		fileCounter: 0,
 	}
 
--- measurement/measurement_test.go
+++ measurement/measurement_test.go
@@ -1,22 +1,60 @@
 package measurement
 
 import (
+	"encoding/csv"
 	"fmt"
+	"math/rand"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/UNH-DistSyS/UNH-CLT/ids"
 )
 
+/***********************************************************************************************************************
+ * Helper functions/variables
+ **********************************************************************************************************************/
+
+var DEBUG = true
+var testPrefix = "test"
+
 func NewMeasurement(listSizeTestParam int) *Measurement {
-	m := CreateMeasurement(ids.GetIDFromFlag(), "test", listSizeTestParam)
-	//view stats
-	fmt.Printf("New Measurement: \n\tNodeId: %d,\n\tcsvPrefix: %s, \n\tlistSize: %d\n", m.clientId.Int(), m.prefix, len(m.data))
-	return m
+	return CreateMeasurement(ids.GetIDFromFlag(), testPrefix, listSizeTestParam)
 }
+
+// Produces random amount of "time" between
+// 10 - 30 mcs to simulate request work
+func SimulateWork() int64 {
+	rand.Seed(time.Now().UnixNano())
+	min := int64(10)
+	max := int64(30)
+
+	return rand.Int63n(max-min+1) + min
+}
+
+func DoMeasurement(m *Measurement, remoteId *ids.ID) (int64, int64) {
+	s := time.Now().UnixMicro()
+	e := s + SimulateWork()
+
+	//add m
+	m.AddMeasurement(int64(100), remoteId, s, e)
+	s -= _startEpoch
+	e -= _startEpoch
+	return s, e
+}
+
+/***********************************************************************************************************************
+ * Test functions
+ **********************************************************************************************************************/
 
 func TestCreateMeasurement(t *testing.T) {
 	listSizeTestParam := 10
 	m := NewMeasurement(listSizeTestParam)
+
+	//view stats
+	if DEBUG {
+		fmt.Printf("New Measurement: \n\tNodeId: %d,\n\tcsvPrefix: %s, \n\tlistSize: %d\n", m.clientId.Int(), m.prefix, len(m.data))
+	}
 
 	if m.fileCounter != 0 {
 		t.Fatalf("Measurement structure file counter not initialized to 0")
@@ -27,16 +65,90 @@ func TestCreateMeasurement(t *testing.T) {
 }
 
 func TestEpoch(t *testing.T) {
-	//TODO: test that epoch is accurate to Jan 1 2023 0:00:00
+	correctTimeMicro := time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC)
+	if DEBUG {
+		fmt.Printf("Epoch time will be equal to %s\n", correctTimeMicro.String())
+	}
+
+	if correctTimeMicro.UnixMicro() != _startEpoch {
+		t.Fatalf("Epoch is %d but should be %d\n", _startEpoch, correctTimeMicro.UnixMicro())
+	}
 }
 
+// IN PROGRESS
 func TestAddMeasurementOnce(t *testing.T) {
-	//listSizeTestParam := 10
-	//fakeNodeId := ids.GetIDFromFlag()
-	//m := NewMeasurement(listSizeTestParam)
+	listSizeTestParam := 10
+	fakeNodeId := ids.GetIDFromFlag()
+	m := NewMeasurement(listSizeTestParam)
 
-	//IN PROGRESS
-	//m.AddMeasurement(int64(100), &fakeNodeId, )
+	s, e := DoMeasurement(m, fakeNodeId)
+
+	entry := m.data[0]
+	if DEBUG {
+		fmt.Printf("Measurement: %d, %d, %d, %d, %d\n", m.clientId.Int(), entry.round, entry.remoteNodeId.Int(), entry.start, entry.end)
+	}
+
+	if entry.remoteNodeId.Int() != fakeNodeId.Int() {
+		t.Fatalf("wrong remote node id recorded")
+	}
+	if entry.start != s {
+		t.Fatalf("wrong start time recorded: %d instead of %d", entry.start, s)
+	}
+	if entry.end != e {
+		t.Fatalf("wrong end time recorded")
+	}
 
 	//if m.data[0]
+}
+
+func TestAddMeasurement100(t *testing.T) {
+	listSizeTestParam := 101
+	fakeNodeId := ids.GetIDFromFlag()
+	m := NewMeasurement(listSizeTestParam)
+
+	for i := 0; i < 100; i++ {
+		s, e := DoMeasurement(m, fakeNodeId)
+
+		entry := m.data[i]
+		if entry.start != s {
+			t.Fatalf("wrong start time recorded: %d instead of %d at entry %d\n", entry.start, s, i)
+		}
+		if entry.end != e {
+			t.Fatalf("wrong end time recorded: %d instead of %d at entry %d\n", entry.end, e, i)
+		}
+
+	}
+}
+
+func TestFlushCSV(t *testing.T) {
+	listSizeTestParam := 100
+	fakeNodeId := ids.GetIDFromFlag()
+	m := NewMeasurement(listSizeTestParam)
+
+	for i := 0; i < 120; i++ {
+		DoMeasurement(m, fakeNodeId)
+	}
+
+	if m.rowCounter != 20 {
+		t.Fatalf("Expected list size %d but got %d\n", 20, m.rowCounter)
+	}
+	if m.fileCounter != 1 {
+		t.Fatalf("Expected file counter size %d but fot %d\n", 1, m.fileCounter)
+	}
+
+	//sleep to allow time to write file
+	time.Sleep(time.Second * 2)
+	f, err := os.Open(testPrefix + "_0.csv")
+	if err != nil {
+		t.Fatalf("error: could not find csv file with appropriate name in current directory\n")
+	}
+	r := csv.NewReader(f)
+	if _, err := r.Read(); err != nil {
+		t.Fatalf("error: empty file\n")
+	}
+
+	rows, _ := r.ReadAll()
+	for i := 0; i < len(rows); i++ {
+		//fmt.Println(rows[i])
+	}
 }
