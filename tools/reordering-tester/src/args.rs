f2620d9161599928899f51e0ccd1f22dd804a8c3
Owen Hilyard <oph1003@unh.edu>
2023-02-17 09:44:02 -0500

    Added reordering testing tool

---
+++ tools/reordering-tester/src/args.rs
@@ -0,0 +1,64 @@
+use std::net::IpAddr;
+
+use clap::Parser;
+use serde::Serialize;
+use strum::{Display, EnumString};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Copy, Display, EnumString, Serialize)]
+pub enum LoggingFormat {
+    #[strum(ascii_case_insensitive)]
+    Tracing,
+    #[strum(ascii_case_insensitive)]
+    Pretty,
+    #[strum(ascii_case_insensitive)]
+    Json,
+}
+
+#[derive(Debug, Parser)]
+pub struct Args {
+    /// A UUID to identify this execution. Usually omitted. Randomized for each execution.
+    #[arg(long, env, default_value_t = Uuid::new_v4())]
+    pub run_id: Uuid,
+
+    /// The destination IPv4 or IPv6 address to send traffic to
+    #[arg(long, env)]
+    pub address: IpAddr,
+
+    /// The destination port
+    #[arg(long, env, default_value_t = 8000)]
+    pub port: u16,
+
+    /// The packet size in bytes to send
+    #[arg(long, env, default_value_t = 800)]
+    pub packet_size: usize,
+
+    /// Pad with random bytes instead of zeros
+    #[arg(long, env, default_value_t = false)]
+    pub enable_random_padding: bool,
+
+    /// How long to send packets for
+    #[arg(long, env, default_value = "60s")]
+    pub test_duration: humantime::Duration,
+
+    /// Desired pause between packets (As fast as possible by default)
+    #[arg(long, env)]
+    pub pause_duration: Option<humantime::Duration>,
+
+    /// The number of threads to spawn to generate packets, must be less than or equal to the number of CPU cores.
+    /// Each will try to send the desired packet configuration.
+    #[arg(long, env, default_value_t = 1)]
+    pub workers: usize,
+
+    /// The logging format
+    #[arg(long, env, default_value_t = LoggingFormat::Tracing)]
+    pub logging_format: LoggingFormat,
+}
+
+pub fn setup_logger(format: &LoggingFormat) {
+    match format {
+        LoggingFormat::Tracing => tracing_subscriber::fmt().init(),
+        LoggingFormat::Pretty => tracing_subscriber::fmt().pretty().init(),
+        LoggingFormat::Json => tracing_subscriber::fmt().json().init(),
+    };
+}
---
+++ tools/reordering-tester/src/server.rs
@@ -0,0 +1,97 @@
+#![deny(unsafe_code)]
+#![deny(clippy::pedantic)]
+
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    net::SocketAddr,
+    sync::Arc,
+    time::Duration,
+};
+
+use clap::Parser;
+use glommio::{net::UdpSocket, prelude::*};
+
+use crate::args::{setup_logger, Args};
+
+pub mod args;
+
+fn main() {
+    let args = Arc::new(Args::parse());
+
+    setup_logger(&args.logging_format);
+
+    tracing::info!(?args);
+
+    let placement = PoolPlacement::MaxSpread(1, None);
+
+    let results = LocalExecutorPoolBuilder::new(placement)
+        .io_memory(2 << 32)
+        .ring_depth(4096)
+        .spin_before_park(Duration::from_secs(1))
+        .on_all_shards(|| async move {
+            let args = args.clone();
+            return server_main(args).await;
+        })
+        .expect("failed to spawn executor")
+        .join_all();
+
+    for result in results {
+        result.unwrap().unwrap();
+    }
+}
+
+type MessageInfoBuf = [u8; 16];
+
+// #[instrument(skip(args))]
+async fn server_main(args: Arc<Args>) -> Result<(), GlommioError<()>> {
+    tracing::info!("Server started");
+
+    let socket_addr = SocketAddr::new(args.address, args.port);
+    let socket = UdpSocket::bind(socket_addr).unwrap();
+
+    let mut results: Vec<MessageInfoBuf> =
+        Vec::with_capacity(args.test_duration.as_millis() as usize * args.workers);
+
+    let mut recv_buffer = vec![0u8; args.packet_size];
+
+    socket.recv_from(&mut recv_buffer).await.unwrap();
+    let mut buf = MessageInfoBuf::default();
+    buf.copy_from_slice(&recv_buffer[..16]);
+    results.push(buf);
+
+    socket
+        .set_read_timeout(Some(Duration::from_secs(5)))
+        .unwrap();
+
+    tracing::info!("Server ready");
+
+    while socket.recv_from(&mut recv_buffer).await.is_ok() {
+        let mut buf = MessageInfoBuf::default();
+        buf.copy_from_slice(&recv_buffer[..std::mem::size_of::<u64>() * 2]);
+        results.push(buf);
+    }
+
+    let results = results.iter_mut().map(|a| {
+        [
+            u64::from_le_bytes([a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]]),
+            u64::from_le_bytes([a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15]]),
+        ]
+    });
+
+    let output_file_name = "out.csv";
+
+    let mut f = BufWriter::new(File::create(output_file_name).unwrap());
+
+    writeln!(f, "client_id,sequence_id").unwrap();
+
+    for [id, seq] in results {
+        writeln!(f, "{id},{seq}").unwrap();
+    }
+
+    f.flush().unwrap();
+
+    tracing::info!("Wrote results to {}", output_file_name);
+
+    Ok(())
+}
